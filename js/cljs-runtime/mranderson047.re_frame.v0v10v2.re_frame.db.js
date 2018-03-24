a34c77dd4d8b4e3da1c690f11fe1f2e219fa369b
Jacek Schae <jacek.schae@gmail.com>
2018-03-24 12:19:09 +0100

    Add reframe-10x to clojure-defines

--- js/cljs-runtime/mranderson047.re_frame.v0v10v2.re_frame.db.js
+++
@@ -1,6 +0,0 @@
-goog.provide('mranderson047.re_frame.v0v10v2.re_frame.db');
-goog.require('cljs.core');
-goog.require('mranderson047.re_frame.v0v10v2.re_frame.interop');
-mranderson047.re_frame.v0v10v2.re_frame.db.app_db = mranderson047.re_frame.v0v10v2.re_frame.interop.ratom(cljs.core.PersistentArrayMap.EMPTY);
-
-//# sourceMappingURL=mranderson047.re_frame.v0v10v2.re_frame.db.js.map
--- js/cljs-runtime/mranderson047.reagent.v0v7v0.reagent.interop.js
+++
@@ -1,4 +0,0 @@
-goog.provide('mranderson047.reagent.v0v7v0.reagent.interop');
-goog.require('cljs.core');
-
-//# sourceMappingURL=mranderson047.reagent.v0v7v0.reagent.interop.js.map
